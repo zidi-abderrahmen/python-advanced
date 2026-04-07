0641f8f0a63e44eabd8d965eb9d105114d9afb8c
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:29:26 +0100

    feat: extract comment

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -7,11 +7,13 @@ def main():
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, "html.parser")
-    # find all elements with class="comment"
-    elements = soup.find_all(class_="comment")
+    # find all elements with class="ind" and indent level = 0
+    elements = soup.find_all(class_="ind", indent=0)
+    # for each of this elements, find the next element
+    comments = [e.find_next(class_="comment") for e in elements]
 
-    # Show the number of elementd found
-    print(f"Elements: {len(elements)}")
+    # Show the number of comments found
+    print(f"Comments: {len(comments)}")
 
 if __name__ == "__main__":
     main()
