2b209407b50d331545ad8e98a4e00c06ef16a77c
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:26:55 +0100

    feat: added the request method

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -1,6 +1,10 @@
+import requests
+
 def main():
     url = "https://news.ycombinator.com/item?id=42919502"
+    response = requests.get(url)
     print(f"Scraping: {url}")
+    print(response)
 
 if __name__ == "__main__":
     main()
