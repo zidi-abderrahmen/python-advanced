9be97479f3242d663db8fa08bfec6983c69ec787
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:18:23 +0100

    feat: importing the entities file and the depenedencies

--- FastAPI with PostgreSQL/main.py
+++ FastAPI with PostgreSQL/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
