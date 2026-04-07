452a7b1222b5cf8e9c058ac5a23aea818312cc0c
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:31:29 +0100

    feat: process the scraped content for useful data

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -12,10 +12,19 @@ def main():
     # for each of this elements, find the next element
     comments = [e.find_next(class_="comment") for e in elements]
 
+    # Map of technologies keyword to search for
+    # and the occurence initialized at 0
+    keywords = {"python": 0, "javascript": 0, "typescript": 0, "go": 0, "c#": 0, "java": 0, "rust": 0}
+
     # show each comment (job post)
     for comment in comments:
-        comment_text = comment.get_text()
-        print(comment_text)
+        # get the comment text and lower case it
+        comment_text = comment.get_text().lower()
+
+        # split comment by space which create an array of words
+        words = comment_text.split(" ")
+
+        print(words)
 
 if __name__ == "__main__":
     main()
