9f0f0ffa0495dee8dc729096208f1069190ee53b
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:22:26 +0100

    feat: added the read quetions and read choices endpoints

--- FastAPI with PostgreSQL/main.py
+++ FastAPI with PostgreSQL/main.py
@@ -40,3 +40,17 @@ def create_questions(question: QuestionBase, db: db_dependency):
         db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
         db.add(db_choice)
     db.commit()
+
+@app.get('/questions/{question_id}')
+def read_questions(question_id: int, db: db_dependency):
+    result = db.query(models.Questions).filter(models.Questions.id == question_id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail='Question is not found!')
+    return result
+
+@app.get('/choices/{question_id}')
+def read_choices(question_id: int, db: db_dependency):
+    result = db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
+    if not result:
+        raise HTTPException(status_code=404, detail='Choices is not found!')
+    return result
