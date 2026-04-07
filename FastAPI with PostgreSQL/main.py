9a281ed404a8eec9c6403e4d92470add2fc194a6
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:10:24 +0100

    feat: created two pydantic base models for Choice and Question

--- FastAPI with PostgreSQL/main.py
+++ FastAPI with PostgreSQL/main.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 from typing import List, Annotated
 
 app = FastAPI()
+
+class ChoiceBase(BaseModel):
+    choice_text: str
+    is_correct: bool
+
+class QuestionBase(BaseModel):
+    question_text: str
+    choices: List[ChoiceBase]
