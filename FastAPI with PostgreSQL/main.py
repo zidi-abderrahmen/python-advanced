88c4dd3670ac5a040b0efa79f47086c70ca6fb77
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:20:57 +0100

    feat: added the question endpoit

--- FastAPI with PostgreSQL/main.py
+++ FastAPI with PostgreSQL/main.py
@@ -26,3 +26,17 @@ def get_db():
 
 # This will be used later for a dependency injection
 db_dependency = Annotated[Session, Depends(get_db)]
+
+# We passing in a data validation to validate the body of the API request
+# and passing the database connection
+@app.post('/questions/')
+def create_questions(question: QuestionBase, db: db_dependency):
+    # Here we use SQL Alchemy to write an ORM statement that will link with the database
+    db_question = models.Questions(question_text=question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+        db.add(db_choice)
+    db.commit()
