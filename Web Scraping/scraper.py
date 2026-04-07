9e28d600b24f1ecef4411b33ffff77cdec6b0ede
zidiabderrahmen <rhoumazaza@gmail.com>
2026-04-07 22:28:29 +0100

    feat: used the BeautifulSoup library

--- Web Scraping/scraper.py
+++ Web Scraping/scraper.py
@@ -1,11 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 def main():
     url = "https://news.ycombinator.com/item?id=42919502"
     response = requests.get(url)
-    print(f"Scraping: {url}")
-    print(response)
-    print(response.content)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    # find all elements with class="comment"
+    elements = soup.find_all(class_="comment")
+
+    # Show the number of elementd found
+    print(f"Elements: {len(elements)}")
 
 if __name__ == "__main__":
     main()
